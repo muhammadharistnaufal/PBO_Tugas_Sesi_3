f4878efa698f17c8f8bd40ad914ecdc61de115a3
muhammadharistnaufal <114551807+muhammadharistnaufal@users.noreply.github.com>
2024-03-20 21:55:59 +0700

    Add files via upload

---
+++ Tugas Pembuatan Kelas Account dan subkelas SavingAccount,CheckingAccount/SavingAccount.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * @author Muhammad Harist Naufal
+ */
+public class SavingAccount extends Account {
+    private double interestRate;
+
+    public SavingAccount(double balance, double interest_rate) {
+        super(balance);
+        this.interestRate = interest_rate;
+    }
+}
